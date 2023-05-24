229d7ee550d90698382ee5917fe1abcae83271c2
Abk123456 <abkarsaifi786@gmail.com>
2023-05-24 22:09:40 +0530

    test

--- Trash/Trash.DAL/TrashDbContext.cs
+++ Trash/Trash.DAL/TrashDbContext.cs
@@ -1,7 +1,23 @@
-﻿namespace Trash.DAL
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace Trash.DAL
 {
     public class TrashDbContext
     {
 
+    }
+    public static class SessionExtensions
+    {
+        public static void SetObjectAsJson(this ISession session, string key, object value)
+        {
+            //session.SetString(key, JsonConvert.SerializeObject(value));
+        }
+
+        //public static T GetObjectFromJson<T>(this ISession session, string key)
+        //{
+        //    //var value = session.GetString(key);
+        //    return key;//value == null ? default(T) : JsonConvert.DeserializeObject<T>(value);
+        //}
     }
 }
